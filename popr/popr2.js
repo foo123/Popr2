f0ed3749c101e036c12fefe70d794d582f0c20a6
Nikos M <nikosms0@gmail.com>
2016-02-28 19:29:44 +0200

    v.2.0.0 (re-upload)
    * content option for dynamic content
    * fix adding extra scrobbar width in IE issue

--- popr/popr2.js
+++ popr/popr2.js
@@ -21,15 +21,15 @@ function viewport( )
 function offset( el )
 {
     // http://stackoverflow.com/a/4689760/3591273
-    var box = el.getBoundingClientRect(),
-        body = document.body, win = window,
+    var body = document.body, win = window,
         clientTop  = body.clientTop  || 0,
         clientLeft = body.clientLeft || 0,
         scrollTop  = win.pageYOffset || body.scrollTop,
         scrollLeft = win.pageXOffset || body.scrollLeft,
+        box = el.getBoundingClientRect(),
         top  = box.top  + scrollTop  - clientTop,
         left = box.left + scrollLeft - clientLeft;
-    return {top: top, left: left, width: el.offsetWidth, height: el.offsetHeight };
+    return {top: top, left: left, width: el.offsetWidth, height: el.offsetHeight/*, swidth: el.scrollWidth, sheight: el.scrollHeight, w: box.width, h: box.height*/ };
 }
 
 var global_handler_added = false;
@@ -37,10 +37,15 @@ var window_resize_added = false;
 
 function popr_adjust( popr, target )
 {
-    var pos = offset( target ), viewPort = viewport( ), 
-        w = popr.outerWidth(), h = popr.outerHeight(),
+    var popr_cont = popr.children('.popr_content')[0],
+        pos = offset( target ), viewPort = viewport( ), 
+        // IE adds scrollbar width in computed width, while FF, Webkit not
+        wScrollbar = popr_cont.scrollWidth > popr_cont.clientWidth
+            ? popr_cont.offsetWidth - popr_cont.clientWidth : 0,
+        w = popr[0].offsetWidth-wScrollbar, h = popr[0].offsetHeight,
         top = pos.top + pos.height, left = pos.left + (pos.width-w)/2
     ;
+    popr.removeClass('popr_container_left popr_container_right');
     if ( popr.hasClass('popr_container_top') || top + h > viewPort.t + viewPort.h )
     {
         top -= pos.height + h;
@@ -63,6 +68,7 @@ function popr_adjust( popr, target )
 $.fn.popr2 = function( options ) {
     var set = $.extend( {
         'selector'     : '.popr',
+        'content'      : null,
         'attribute'    : 'data-popr',
         'activate'     : 'click',
         'class'        : '',
@@ -85,15 +91,15 @@ $.fn.popr2 = function( options ) {
     $(this).on(activate_event, set.selector, function(event) {
         event.stopPropagation( );
         if ( activate_type !== event.type ) return false;
-        var el = this, $el = $(el), popr_class, attr_class = '', attr_mode, d_m;
+        var el = this, $el = $(el), content, popr_class, attr_class = '', attr_mode, d_m;
         $('.popr_container').remove( );
 
         d_m = set.mode;
         if ( !!(attr_mode = $el.attr('data-popr-mode')) ) d_m = attr_mode;
         attr_class = $el.attr('data-popr-class');
         popr_class = 'popr_container popr_container_' + d_m + (!!set['class'] ? ' '+set['class'] : '') + (!!attr_class ? ' '+attr_class : '');
-
-        var popr = $('<div class="'+popr_class+'"><div class="popr_content">' + $('#' + $el.attr(set.attribute)).html( ) + '</div></div>').appendTo('body');
+        content = !!set.content ? ('function' === typeof set.content ? set.content( el ) : set.content) : $('#' + $el.attr(set.attribute)).html( );
+        var popr = $('<div class="'+popr_class+'"><div class="popr_content">'+content+'</div></div>').appendTo('body');
         popr[0]._popr_target = el;
         
         popr_adjust( popr, el );
@@ -104,14 +110,13 @@ $.fn.popr2 = function( options ) {
                 // outside click or ESC key pressed
                 var $el = $(evt.target);
                 if ( ('keyup' === evt.type && 27 === evt.which) ||
-                    ('mouseup' === evt.type && (($el.is('a,label,.popr-item') && $el.parent('.popr_content').length) || !$el.closest('.popr_container').length)) 
+                    ('mouseup' === evt.type && (($el.is('a,label,button,.popr-item') && $el.parent('.popr_content').length) || !$el.closest('.popr_container').length)) 
                 )
                 {
                     $('body').off('mouseup.popr2 keyup.popr2');
                     global_handler_added = false;
-                    setTimeout(function( ) {
-                        $('.popr_container').remove( );
-                    }, 10);
+                    // slight delay
+                    setTimeout(function( ){ $('.popr_container').remove( ); }, 10);
                 }
             });
             global_handler_added = true;
@@ -123,9 +128,10 @@ $.fn.popr2 = function( options ) {
     if ( !window_resize_added )
     {
         $(window).on('resize.popr2', function popr_resize( evt ){
-            $('.popr_container').each(function( ){
-                popr_adjust( $(this), this._popr_target );
-            });
+            // slight delay
+            setTimeout(function( ){
+            $('.popr_container').each(function( ){ popr_adjust( $(this), this._popr_target ); });
+            }, 10);
         });
         window_resize_added = true;
     }
